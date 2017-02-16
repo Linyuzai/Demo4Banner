5d5847cc57698331eb2f8d402338748da71aa2a9
linyuzai <120718461@qq.com>
2017-02-16 14:39:06 +0800

    add ImageBannerAdapter2

---
+++ banner/src/main/java/com/linyuzai/banner/adapter/ImageBannerAdapter2.java
@@ -0,0 +1,44 @@
+package com.linyuzai.banner.adapter;
+
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.linyuzai.banner.BannerAdapter2;
+import com.linyuzai.banner.ViewHolder;
+
+/**
+ * Created by Administrator on 2017/2/16 0016.
+ *
+ * @author Linyuzai
+ */
+
+public abstract class ImageBannerAdapter2 extends BannerAdapter2 {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent) {
+        ImageView view = new ImageView(parent.getContext());
+        onImageViewCreated(view);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        onBindImage((ImageView) holder.itemView, position);
+    }
+
+    /**
+     * 用来设置ImageView的属性
+     *
+     * @param view 被创建的ImageView
+     */
+    public abstract void onImageViewCreated(ImageView view);
+
+    /**
+     * 绑定数据
+     *
+     * @param image    需要绑定数据的ImageView
+     * @param position HintView item的position
+     */
+    public abstract void onBindImage(ImageView image, int position);
+}
+
